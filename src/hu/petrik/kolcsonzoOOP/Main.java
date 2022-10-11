00c5888d8197e378da18bc62cd4fe4bde05006c8
Morgován Nimród <morgovanimrod22@gmail.com>
2022-10-11 15:35:00 +0200

    tesztelések

--- src/hu/petrik/kolcsonzoOOP/Main.java
+++ src/hu/petrik/kolcsonzoOOP/Main.java
@@ -1,20 +1,25 @@
 package hu.petrik.kolcsonzoOOP;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
     static List<Kolcsonozheto> katalogus = new ArrayList<>();
+    static List<Dvd> dvdk = new ArrayList<>();
 
     public static void main(String[] args) {
-    Konyv konyv1 = new Konyv(" A Gyűrűk Ura","J. R. R. Tolkien","AABBCC-121314");
-    Dvd dvd1 = new Dvd("Star Wars V. rész - A Birodalom visszavág", 124);
+        Konyv konyv1 = new Konyv(" A Gyűrűk Ura", "J. R. R. Tolkien", "AABBCC-121314");
+        Dvd dvd1 = new Dvd("Star Wars V. rész - A Birodalom visszavág", 124);
         Dvd dvd2 = new Dvd("Star Wars VI. rész - A jedi visszatér", 131);
-        Dvd dvd3 = new Dvd("Star Wars IV. rész - Az új remény", 121);
-        Ujsag ujsag1 = new Ujsag("Képes sport",2007,23);
+        Dvd dvd3 = new Dvd("Star Wars IV. rész - Egy új remény", 121);
+        Dvd dvd4 = new Dvd("A Gyűrűk Ura - A gyűrű szövetsége", 201);
+        Dvd dvd5 = new Dvd("A Gyűrűk Ura - A király tornya", 164);
+        Dvd dvd6 = new Dvd("Batman under red hood", 121);
+        Ujsag ujsag1 = new Ujsag("Képes sport", 2007, 23);
 
-        // Interface
+        // Interface létrehozása lehetséges
         Kolcsonozheto kolcsonozheto = new Kolcsonozheto() {
             @Override
             public String megjelenitendoNev() {
@@ -32,11 +37,31 @@ public class Main {
         katalogus.add(dvd1);
         katalogus.add(dvd2);
         katalogus.add(dvd3);
+        katalogus.add(dvd4);
+        katalogus.add(dvd5);
+        katalogus.add(dvd6);
         katalogus.add(ujsag1);
+        dvdk.add(dvd1);
+        dvdk.add(dvd2);
+        dvdk.add(dvd3);
+        dvdk.add(dvd4);
+        dvdk.add(dvd5);
+        dvdk.add(dvd6);
 
         for (Kolcsonozheto value : katalogus) {
             System.out.println(value.megjelenitendoNev() + " " + value.meddigKolcsonozheto());
         }
 
+        System.out.println("A DVD-k rendezés előtt");
+        for (Dvd d: dvdk) {
+            System.out.println(d.megjelenitendoNev()+" "+d.getHossz());
+        }
+        System.out.println();
+        Collections.sort(dvdk);
+        System.out.println("A DVD-k rendezés után");
+        for (Dvd d: dvdk) {
+            System.out.println(d.megjelenitendoNev()+" "+d.getHossz());
+        }
+        System.out.println();
     }
 }
